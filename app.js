04cff47776a3e3ef3814a29a9a8d18785f839882
Nena Basina <basina@ics.forth.gr>
2020-11-11 17:30:07 +0200

    intermediate commit

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const bodyParser= require('body-parser');
 const methods = require('./methods');
 
 const port = process.env.port || 80;
+const hostname = '139.91.183.118';
 
 app.listen(port, () => {
-    console.log(`Chatbot Server is listening at port:${port}`);
+    console.log(`Chatbot Server is listening at     http://${hostname}:${port}`);
 })
 
 // all files inside public are static and available to the frontend
@@ -42,3 +43,55 @@ app.post('/', (req, res, next) => {
         bot.sendText(`You said: ${msgObject.message}`, msgObject.id)
     }
 })
+
+// =============================================================================
+// ROUTES FOR OUR API
+// =============================================================================
+const router = express.Router();
+
+// middleware to use for all requests
+// we want sth to happen every time a request is sent to our API
+router.use(function(req, res, next) {
+    // logging
+    console.log('Something is happening... New route request');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test router (accessed at GET http://localhost:80/api)
+router.get('/', function(req, res) {
+    console.log("---> API CALL\t",req.session.id);
+    res.write(`<h1>Welcome to the API!</h1>`);
+    res.end();
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+//// on routes that end in /chatbot
+// --------------------------------------------------------------------------------------------
+router.route('/chatbot')
+// (accessed at GET http://localhost:80/api/chatbot)
+.get((req, res) => {
+    // this is sent to the frontend jquery call as the data var of the request
+    res.send("The server side of the chatbot has been accessed");
+});
+
+//// on routes that end in /chatbot/getUserMsg
+// --------------------------------------------------------------------------------------------
+router.route('/chatbot/getUserMsg')
+// (accessed at GET http://localhost:80/api/chatbot/getUserMsg)
+.get((req, res) => {
+    res.send("got user msg");
+});
+
+//// on routes that end in /chatbot/getBotMsg
+// --------------------------------------------------------------------------------------------
+router.route('/chatbot/getBotMsg')
+// (accessed at GET http://localhost:80/api/chatbot/getBotMsg)
+.get((req, res) => {    // first receive from UI server
+    res.send("received msg from UI server");
+})
+.post((req, res) => {    // then send to chatbot frontend
+    res.send("got bot msg");
+});
--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,14 @@ function scrollContents(container) {
     container.scrollTop = container.scrollHeight;
 }
 
-/**
+var responseMsg = "";
+
+
+/*****************************************************************************
+ * USER MESSAGES
+ *****************************************************************************/
+
+/******* Add user message
  * Create a DOM element from a string value
  * @function getMessageElement 
  * @param {string} val
@@ -22,11 +29,11 @@ function scrollContents(container) {
 */
 function getMessageElement(val, isUser) {
     // Create parent message element to append all inner elements
-    var newMessage = document.createElement('div');
+    var newUserMessage = document.createElement('div');
     
     // Add message variation class when the message belongs to the user
     if (isUser) {
-        newMessage.className += 'chat-message--right ';
+        newUserMessage.className += 'chat-message--right ';
     }
 
     // Create text
@@ -35,37 +42,75 @@ function getMessageElement(val, isUser) {
     text.className += 'chat-message__text';
 
     // Append elements
-    newMessage.append(text);
-    newMessage.className += 'chat-message ';
+    newUserMessage.append(text);
+    newUserMessage.className += 'chat-message ';
 
-    return newMessage;
+    return newUserMessage;
 }
 
+// add user message to UI
+function addUserMsg(msg) {
+    // Add user's message to the chat log
+    var newUserMessage = getMessageElement(msg, true);
+    chatLog[0].append(newUserMessage);
+    // Scroll to last message
+    scrollContents(chatLog[0]);
+}
+
+// jquery request to get user msg (if exists) from the Server
+var receiveUserMsg = () => $.get("./api/chatbot/getUserMsg")
+.fail((err)=>{
+    console.error(err,": Failed to receive user message from chatbot server");
+})
+.done((data)=>{
+    console.log('success');
+    console.log("User msg:\n",data);
+    addUserMsg(data);
+})
 
 // Handle form submit (clicking on the submit button or pressing Enter)
 chatForm[0].addEventListener('submit', function(e) {
     e.preventDefault();
 
     // If reply is loading, wait
-    if (loading) {
-        return false;
-    }
+    if (loading) { return false; }
 
     // Catch empty messages
-    if (!chatInputField[0].value) {
-        return false;
-    }
+    if (!chatInputField[0].value) { return false; }
 
     // Add user's message to the chat log
-    var newMessage = getMessageElement(chatInputField[0].value, true);
-    chatLog[0].append(newMessage);
+    addUserMsg(chatInputField[0].value);
 
-    // Scroll to last message
-    scrollContents(chatLog[0]);
-
-    // Get the reply from dialogflow.com
+    // Get the reply from wit.ai
     // getReply(chatInputField[0].value);
 
     // Clear input
     chatInputField[0].value = '';
+
+    receiveUserMsg();
+    receiveBotMsg();
 });
+
+
+/*****************************************************************************
+ * CHATBOT MESSAGES/REPLIES
+ *****************************************************************************/
+// jquery request to get bot msg from the Server
+var receiveBotMsg = () => $.post("./api/chatbot/getBotMsg")
+.fail((err)=>{
+    console.error(err,": Failed to receive bot message from chatbot server");
+})
+.done((data)=>{
+    console.log('success');
+    console.log("Bot msg:\n",data);
+    addBotMsg(data);
+})
+
+// add bot message to UI
+function addBotMsg(msg) {
+    // Add user's message to the chat log
+    var newBotMessage = getMessageElement(msg, false);  // true is user, false is bot
+    chatLog[0].append(newBotMessage);
+    // Scroll to last message
+    scrollContents(chatLog[0]);
+}
